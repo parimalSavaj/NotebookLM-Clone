809b456db5cb959557e63820a35c93e445ab8102
savaj parimal <parimalsavaj01@gmail.com>
2026-08-04 22:19:20 +0530

    set-up

---
+++ server/src/index.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
